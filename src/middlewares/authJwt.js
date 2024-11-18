8f6d044180ed52345dc17ab62f1c88a11d8b4297
syyoon <ounsy0612@gmail.com>
2024-11-18 11:49:54 +0900

    responseHelper 코드값 추가.

--- src/middlewares/authJwt.js
+++ src/middlewares/authJwt.js
@@ -11,16 +11,10 @@ const authJWT = (req, res, next) => {
       next();
     } else {
       // 검증에 실패하거나 토큰이 만료되었다면 클라이언트에게 메세지를 담아서 응답합니다.
-      res.status(401).send({
-        ok: false,
-        message: result.message, // jwt가 만료되었다면 메세지는 'jwt expired'입니다.
-      });
+      res.sendError(401, result.message)
     }
   } else {
-    res.status(401).send({
-      ok: false,
-      message: "토큰이 없습니다.", // jwt가 만료되었다면 메세지는 'jwt expired'입니다.
-    });
+    res.sendError(401, '토큰이 없습니다.')// jwt가 만료되었다면 메세지는 'jwt expired'입니다.
   }
 };
 
