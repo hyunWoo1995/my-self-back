74966e0f543a1bec60e11ac97a3f8c5c7f5bf37b
leehyunwoo <archihw94@gmail.com>
2024-11-27 15:05:37 +0900

    modify

--- src/model/moimModel.js
+++ src/model/moimModel.js
@@ -91,7 +91,7 @@ exports.modifyActiveTime = async ({ meetings_id, users_id }) => {
 // 메세지 전체 조회
 exports.getMessages = async ({ meetings_id, length }) => {
   const [lists] = await db.query(
-    "SELECT m.id, m.contents, m.created_at, m.users_id, m.meetings_id, m.users, u.nickname  FROM moimmoim.messages AS m join users u on m.users_id = u.id where meetings_id = ? ORDER BY  m.created_at DESC;",
+    "SELECT m.id, m.contents, m.created_at, m.users_id, m.meetings_id, m.users, u.nickname  FROM moimmoim.messages AS m left join users u on m.users_id = u.id where meetings_id = ? ORDER BY  m.created_at DESC;",
     [meetings_id]
   );
 
@@ -119,7 +119,7 @@ exports.getMessages = async ({ meetings_id, length }) => {
 
 // 메세지 단일 조회
 exports.getMessage = async (meetings_id, id, usersInRoom) => {
-  const [rows] = await db.query("select m.*, u.nickname from messages m join users u on m.users_id = u.id where meetings_id = ? and m.id= ?", [meetings_id, id]);
+  const [rows] = await db.query("select m.*, u.nickname from messages m left join users u on m.users_id = u.id where meetings_id = ? and m.id= ?", [meetings_id, id]);
 
   // const [meetingsUsers] = await db.query("select * from meetings_users where meetings_id = ? and status = 1", [meetings_id]);
 
