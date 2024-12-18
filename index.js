1811add96dbe0df746fb57c95e5ed50117dd2d83
leehyunwoo <archihw94@gmail.com>
2024-12-18 22:21:24 +0900

    modify auth middleware

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ app.use(
 app.use((req, res, next) => {
   // 제외할 경로 리스트
   const excludedPaths = ["/"];
-  const excludedPrefixes = ["/auth", "/public"]; // auth로 시작하는 경로 전체
+  const excludedPrefixes = ["/auth", "/assets"]; // auth로 시작하는 경로 전체
   console.log("req.path", req.path);
 
   const isExcludedPath = excludedPaths.includes(req.path);
